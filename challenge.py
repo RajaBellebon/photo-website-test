e7a22de12d7609bbdb4c5bc3850528f69a159fdc
christbel <bellebonchristian@gmail.com>
2020-11-01 18:09:47 +0100

    Twinkle poem

--- challenge.py
+++ challenge.py
@@ -1,2 +1,10 @@
 
-print ("Hello")
+
+ver1 ='Twinkle, twinkle, little star,'
+ver2 = '\n \tHow I wonder what you are!' 
+ver3 = '\n \t \tUp above the world so high,'
+ver4 = '\n \t \tLike a diamond in the sky.'
+ver5 = '\nTxinkle, twinkle,little star,' 
+ver6 = '\n \tHow I wonder what you are.'
+output = ver1 + "" + ver2 + ver3 + "" + ver4 + ver5 + "" +ver6
+print (output)
