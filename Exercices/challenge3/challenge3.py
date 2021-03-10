10d30de7cd937a77c74d24f328b21bfd77c700a9
christbel <bellebonchristian@gmail.com>
2021-03-10 16:49:19 +0100

    Modification color_liste

--- Exercices/challenge3/challenge3.py
+++ Exercices/challenge3/challenge3.py
@@ -3,3 +3,16 @@
 #### Write a Python program to display the first and last colors from the following list.
 
 #### color_list = ["Red","Green","White" ,"Black"]
+
+
+color_list = ["Red","Green","White","Black"]
+print(color_list)
+del color_list[1:2]
+del color_list[1:2]
+print(color_list)
+
+
+list_1 = ["Red","Green","White" ,"Black"]
+def first_last(list_1): 
+    print("first element is"+list_1[0]+"last element is"+[len(list_1)-1])
+    
