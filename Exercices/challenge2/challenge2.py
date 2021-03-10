271f49686d79a693cc1842ade9ac91d5f039e99f
christbel <bellebonchristian@gmail.com>
2021-03-10 16:57:48 +0100

    Current date and time

--- Exercices/challenge2/challenge2.py
+++ Exercices/challenge2/challenge2.py
@@ -3,3 +3,9 @@
 ### Write a Python program to display the current date and time.
 
 ### Helper: https://docs.python.org/3/library/datetime.html
+
+
+import datetime
+now = datetime.datetime.now ()
+dateStr = now.strftime("%Y-%m-%d-%d %H:%M:%S")
+print(f"Date and local Time into Djerba :{dateStr}")
