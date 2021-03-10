6ae391462ebf1696eed1884fbef3f9e0f7520945
christbel <bellebonchristian@gmail.com>
2021-03-10 16:54:53 +0100

    Check a specified value

--- Exercices/challenge4/challenge4.py
+++ Exercices/challenge4/challenge4.py
@@ -5,3 +5,19 @@
 ### 3 -> [1, 5, 8, 3] : True
 ###-1 -> [1, 5, 8, 3] : False
 ###
+
+#list_1 = [1, 5, 8, 3]
+#element_count= list_1.count ([5])
+#print(element_count)
+
+#list_1 = [1,5,8,3]
+#print(list_1[len(list_1)-1])
+
+
+list_1 = [1,5,8,3]
+element_count= list_1.count ([len(list_1)-1])
+print ('the count of element 3 is', element_count)
+if element_count >= 1 :
+    print (3 is )
+
+
